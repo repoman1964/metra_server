02e2eeba92c99590e450c027ba98421d65287729
Jose <jmadarieta3@gmail.com>
2024-05-15 23:17:07 -0400

    changed database to postgres

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,12 +82,25 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metra_db',
+        'USER': 'metra_db_user',
+        'PASSWORD': 'O5ba5fFN%EC2pHTfYD^eYsJZqkW3B',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 
 
 # Password validation
